7d22af6cfa070f551dfe3694982825c2669ba9b9
Cyich <15079005772@163.com>
2020-02-25 11:10:48 +0800

    add index and BootStrap

---
+++ src/main/java/file/majiang/community/controller/IndexController.java
@@ -0,0 +1,15 @@
+package file.majiang.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by Yuqi Xi on 2020/2/22.
+ */
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(){ return "index"; }
+}
